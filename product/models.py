a842dd7d519a99ee7e5d87f3e7e31355eb3cbdb4
slayer <aakashsagar640@gmail.com>
2020-08-23 22:55:03 +0530

    test

--- product/models.py
+++ product/models.py
@@ -14,7 +14,7 @@ from django.db import models
 #     category=models.CharField(max_length=20,null=True,default=True)
 #     def __str__(self): 
 # 	    return self.title 
-# # declare a new model with a name "GeeksModel" 
+
 
 class Blog(models.Model): 
     heading = models.CharField(max_length = 100,null=True,default=True) 
